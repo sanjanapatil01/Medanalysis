4dd991fc0df92234e406d318cc2c54b1f84d48e1
Suhen02 <suhels19914@gmail.com>
2025-04-13 16:02:12 +0530

    ready to predict

--- MedAnalysis/settings.py
+++ MedAnalysis/settings.py
@@ -43,13 +43,14 @@ ROOT_URLCONF = 'MedAnalysis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.csrf',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
